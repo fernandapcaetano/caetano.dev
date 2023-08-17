c5a26b1e6c5cd5980ea54a9b3d92505428aa55ff
Nanda P Caetano <fernandamariacaetanopereira@gmail.com>
2023-08-17 15:08:35 -0300

    js

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+menu = document.querySelector(".menu");
+menu.addEventListener("click", animacao_menu);
+
+function animacao_menu() {
+
+    // quando clicar, fazer as barras se juntarem
+
+
+
+
+    menu.classList.toggle("menu_clicado");
+}
